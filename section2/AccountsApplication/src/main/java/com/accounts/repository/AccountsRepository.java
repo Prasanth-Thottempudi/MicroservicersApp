2bbc114e0db68d569bb1cecf787cffe964e95e99
prasanth <tprasanth678@gmail.com>
2025-01-18 23:54:17 +0530

    added docker file in section 4 in user module

--- section2/AccountsApplication/src/main/java/com/accounts/repository/AccountsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.accounts.repository;
-
-import com.accounts.entity.Accounts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountsRepository extends JpaRepository<Accounts,Long> {
-}
